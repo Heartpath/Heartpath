6dcb06588da9ba2ca0dcd30fe34a852160bacd02
dongHyeok <mimiga1@naver.com>
2023-10-24 17:55:31 +0900

    ✨ feat : 애니메이션 & 상태바 색 변경 | 하동혁

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/home/HomeFragment.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/home/HomeFragment.kt
@@ -3,6 +3,8 @@ package com.zootopia.presentation.home
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.zootopia.presentation.MainActivity
 import com.zootopia.presentation.R
 import com.zootopia.presentation.config.BaseFragment
@@ -12,6 +14,7 @@ class HomeFragment :
     BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
     
     private lateinit var mainActivity: MainActivity
+    private lateinit var navController: NavController
     
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -20,8 +23,21 @@ class HomeFragment :
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initClickEvent()
     }
     
-    private fun initView() = with(binding) {
+    private fun initClickEvent() = with(binding) {
+        buttonLetterList.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_receiveLetterFragment)
+        }
+        
+        buttonLetterStorage.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_sendLetterFragment)
+        }
+        
+        buttonMap.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_mapFragment)
+        }
+    
     }
 }
