91949a7f051a18591fbe42df04fc884738743f12
dongHyeok <mimiga1@naver.com>
2023-10-24 14:13:54 +0900

    ✨ feat : navigation 부분완성 | 하동혁

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/MainActivity.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.zootopia.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.zootopia.presentation.config.BaseActivity
 import com.zootopia.presentation.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     
+    lateinit var navController: NavController
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    
+        val navHostFragmentManager = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragmentManager.navController
     }
 }
