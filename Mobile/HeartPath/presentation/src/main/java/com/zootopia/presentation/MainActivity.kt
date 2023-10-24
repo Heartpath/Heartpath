3b1cdcf83495ce415ef68e406d129ad4cd783aeb
dongHyeok <mimiga1@naver.com>
2023-10-24 13:07:43 +0900

    📃 docs : BaseActivity, Fragment 생성 | 하동혁

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/MainActivity.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/MainActivity.kt
@@ -2,8 +2,11 @@ package com.zootopia.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.zootopia.presentation.config.BaseActivity
+import com.zootopia.presentation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
