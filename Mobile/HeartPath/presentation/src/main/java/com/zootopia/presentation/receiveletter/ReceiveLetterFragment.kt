6d8da6798acd0592ee13c31205b489c295b71212
dongHyeok <mimiga1@naver.com>
2023-10-24 16:40:11 +0900

    ✨ feat : 보낸편지함 UI 완성 | 하동혁

---
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/receiveletter/ReceiveLetterFragment.kt
@@ -0,0 +1,52 @@
+package com.zootopia.presentation.receiveletter
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
+import com.zootopia.presentation.MainActivity
+import com.zootopia.presentation.R
+import com.zootopia.presentation.config.BaseFragment
+import com.zootopia.presentation.databinding.FragmentReceiveLetterBinding
+
+private const val TAG = "ReceiveLetterFragment_HeartPath"
+class ReceiveLetterFragment : 
+    BaseFragment<FragmentReceiveLetterBinding>(FragmentReceiveLetterBinding::bind, R.layout.fragment_receive_letter) {
+    
+    private lateinit var mainActivity: MainActivity
+    private lateinit var navController: NavController
+    private lateinit var receiveLetterAdapter: ReceiveLetterAdapter
+    
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        
+        initAdapter()
+    }
+    
+    private fun initAdapter() = with(binding) {
+        receiveLetterAdapter = ReceiveLetterAdapter().apply {
+            itemClickListener = object : ReceiveLetterAdapter.ItemClickListener {
+                override fun itemClick(view: View, position: Int) {
+                    Log.d(TAG, "itemClick: 편지 리스트 클릭됨 $position")
+                }
+            }
+        }
+        
+        recyclerviewReceiveLetter.apply {
+            adapter = receiveLetterAdapter
+            layoutManager = GridLayoutManager(mainActivity, 2, GridLayoutManager.VERTICAL, false)
+        }
+    }
+    
+    private fun initView() = with(binding) {
+    }
+}
