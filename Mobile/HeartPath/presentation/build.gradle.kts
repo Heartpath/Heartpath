e4125af9021f1874e47fd329d35d373b0aac2122
dongHyeok <mimiga1@naver.com>
2023-10-25 10:04:17 +0900

    ✨ feat : Naver map 의존성 추가 | 하동혁

--- Mobile/HeartPath/presentation/build.gradle.kts
+++ Mobile/HeartPath/presentation/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.compose.material3:material3:1.0.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.10.0")
@@ -79,4 +78,7 @@ dependencies {
     // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    
+    // 네이버 지도 SDK
+    implementation("com.naver.maps:map-sdk:3.17.0")
 }
