028b0f3379f815d10036de34b20a696fd223f2a9
dongHyeok <mimiga1@naver.com>
2023-10-27 18:04:27 +0900

    ✨ feat: 네이버 지도 길찾기 전까지 구현 | 하동혁

---
+++ Mobile/HeartPath/data/src/main/java/com/zootopia/data/datasource/service/NaverMapService.kt
@@ -0,0 +1,12 @@
+package com.zootopia.data.datasource.service
+
+interface NaverMapService {
+//    @GET("/map-direction/v1/driving")
+//    fun getDrivingDirections(
+//        @Query("start") startLocation: String,
+//        @Query("goal") goalLocation: String,
+//        @Query("option") option: String,
+//        @Header("X-NCP-APIGW-API-KEY-ID") apiKeyId: String,
+//        @Header("X-NCP-APIGW-API-KEY") apiKey: String
+//    ): Call<YourResponseClass> // Replace with your response model class
+}
