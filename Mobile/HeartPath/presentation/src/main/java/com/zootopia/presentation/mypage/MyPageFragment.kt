ba7b72b381f8ae5bf8c0bfa7c9b1c00c355011f6
황신운 <brandy.hwang@gmail.com>
2023-10-27 10:00:22 +0900

    ✏ modify : 마이 페이지 수정 버튼 클릭 연결, recyclerview 연결

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/mypage/MyPageFragment.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/mypage/MyPageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.zootopia.presentation.R
 import com.zootopia.presentation.config.BaseFragment
 import com.zootopia.presentation.databinding.FragmentMyPageBinding
@@ -13,4 +15,27 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding> (
     FragmentMyPageBinding::bind,
     R.layout.fragment_my_page
 ){
+    private lateinit var myPageFriendAdapter: MyPageFriendAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        initClickEvent()
+
+    }
+    private fun initAdapter() = with(binding) {
+        myPageFriendAdapter = MyPageFriendAdapter()
+
+    }
+    private fun initClickEvent() = with(binding) {
+        linearlayoutAddFriend.setOnClickListener {
+            findNavController().navigate(R.id.action_myPageFragment_to_friendSearchFragment)
+        }
+        linearlayoutPoint.setOnClickListener {
+            findNavController().navigate(R.id.action_myPageFragment_to_pointHistoryFragment)
+        }
+        recyclerviewMypageFriend.apply {
+            adapter = myPageFriendAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+    }
 }
