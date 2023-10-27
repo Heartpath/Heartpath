1202e8dbd8b8ebe3df3e90b0f9b0d08cca5d06a9
황신운 <brandy.hwang@gmail.com>
2023-10-27 12:38:40 +0900

    ✨ feat : bottom dialog 구현 친구 추가, 친구 삭제, 사용자 신고 dialog 구현

---
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/searchfriend/FriendSearchFriendAddDialog.kt
@@ -0,0 +1,8 @@
+package com.zootopia.presentation.searchfriend
+
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class FriendSearchFriendAddDialog() : BottomSheetDialogFragment() {
+
+}
