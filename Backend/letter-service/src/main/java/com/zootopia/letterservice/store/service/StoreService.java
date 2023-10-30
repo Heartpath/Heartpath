230ce6154d4ce70b25c1a9da3dddf4d20e31001b
HYEJIN <vvloveletter@naver.com>
2023-10-30 17:59:56 +0900

    ✨ feat : 캐릭터 구매 기능 일부 구현

--- Backend/letter-service/src/main/java/com/zootopia/letterservice/store/service/StoreService.java
+++ Backend/letter-service/src/main/java/com/zootopia/letterservice/store/service/StoreService.java
@@ -1,5 +1,6 @@
 package com.zootopia.letterservice.store.service;
 
+import com.zootopia.letterservice.store.dto.request.CharacterBuyReqDto;
 import com.zootopia.letterservice.store.dto.request.LetterPaperBuyReqDto;
 import com.zootopia.letterservice.store.entity.CrowTit;
 import com.zootopia.letterservice.store.entity.LetterPaper;
@@ -12,6 +13,8 @@ public interface StoreService {
     void buyLetterPaper(String memberId, LetterPaperBuyReqDto letterPaperBuyReqDto);
 
     LetterPaper getLetterPaperDetail(Long letterpaperId);
+
+    void buyCharacter(String memberId, CharacterBuyReqDto characterBuyReqDto);
 
     CrowTit getCharacterInfo(Long charater_id);
 }
