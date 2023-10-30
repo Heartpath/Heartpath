d1d2f81d7176545cf8f4c5ff2e74c3f3c66f7375
김민수 <mskim.spring@gmail.com>
2023-10-30 14:12:10 +0900

    ✨ feat : 유저 정보 조회

--- Backend/user-service/src/main/java/com/zootopia/userservice/controller/UserController.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/controller/UserController.java
@@ -1,10 +1,30 @@
 package com.zootopia.userservice.controller;
 
+import com.zootopia.userservice.common.BaseResponse;
+import com.zootopia.userservice.dto.UserInfoDTO;
+import com.zootopia.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
 @RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/mypage")
+    public ResponseEntity<BaseResponse> getUserInfo() {
+
+        UserInfoDTO userInfoDTO = userService.loadUserInfo("SSAFY");
+
+        return ResponseEntity.status(200).body(new BaseResponse(HttpStatus.OK, "유저 불러오기", userInfoDTO));
+    }
 }
