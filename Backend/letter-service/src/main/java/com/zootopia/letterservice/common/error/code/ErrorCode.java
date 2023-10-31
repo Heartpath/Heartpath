9054b1e57ad525d5243427bd9304d36972e816a5
HYEJIN <vvloveletter@naver.com>
2023-10-31 17:05:10 +0900

    ✨ feat : 캐릭터 구매 기능 저장 일부 구현

--- Backend/letter-service/src/main/java/com/zootopia/letterservice/common/error/code/ErrorCode.java
+++ Backend/letter-service/src/main/java/com/zootopia/letterservice/common/error/code/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     NOT_EXISTS_LETTERPAPER(HttpStatus.BAD_REQUEST, "LP-001", "존재하지 않는 편지지 입니다."),
 
     // CrowTit
-    NOT_EXISTS_CROWTIT(HttpStatus.BAD_REQUEST, "CT-001", "존재하지 않는 뱁새입니다.");
+    NOT_EXISTS_CROWTIT(HttpStatus.BAD_REQUEST, "CT-001", "존재하지 않는 뱁새입니다."),
+    FAIL_SEND_TO_MEMBER_CROWTIT(HttpStatus.INTERNAL_SERVER_ERROR, "CT-002", "멤버서버로 뱁새 정보 전송에 실패하였습니다.");
 
     private HttpStatus httpStatus;
     private String errorCode;
