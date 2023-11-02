bbdc505ae43b047226c512dc9f3e0c6706e18feb
황신운 <brandy.hwang@gmail.com>
2023-11-02 16:11:43 +0900

    ✏ modify : setting bgm dialog 수정 dialog switch 수정, dialog 화면 수정

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/settings/BgmSettingDialog.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/settings/BgmSettingDialog.kt
@@ -2,17 +2,24 @@ package com.zootopia.presentation.settings
 
 import android.app.Dialog
 import android.content.Context
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.DialogFragment
+import com.zootopia.presentation.R
 import com.zootopia.presentation.databinding.DialogBgmSettingBinding
 
-class BgmSettingDialog(): DialogFragment() {
+class BgmSettingDialog: DialogFragment() {
     private lateinit var binding: DialogBgmSettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.Dialog_Custom_Style)
+        isCancelable = true // 화면 밖에 클릭하면 dismiss 되도록
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,18 +27,25 @@ class BgmSettingDialog(): DialogFragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DialogBgmSettingBinding.inflate(inflater, container, false)
-        val view = binding.root
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.apply {
             buttonDone.setOnClickListener {
-            //TODO: 상태 저장
+                //TODO: 상태 저장
                 dismiss()
             }
         }
-        return view
+    }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.window?.setGravity(Gravity.BOTTOM) // 다이얼로그를 하단에 표시
+        return dialog
     }
 
-    interface BgmSettingDialogListener {
-        fun onClick(dialog: BgmSettingDialog)
+    companion object {
+        const val TAG = "BgmSettingDialog"
     }
 }
