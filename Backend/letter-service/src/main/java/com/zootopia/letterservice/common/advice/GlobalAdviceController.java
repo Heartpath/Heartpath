7340080a3cf7566560d9e746772c48a7edf893f9
YEONJI <kkgg54@naver.com>
2023-11-02 13:17:40 +0900

    ✏ modify : letter, store 서비스 분리

--- Backend/letter-service/src/main/java/com/zootopia/letterservice/common/advice/GlobalAdviceController.java
+++ Backend/letter-service/src/main/java/com/zootopia/letterservice/common/advice/GlobalAdviceController.java
@@ -1,6 +1,6 @@
 package com.zootopia.letterservice.common.advice;
 
-import com.zootopia.letterservice.common.error.exception.LetterBadRequestException;
+import com.zootopia.letterservice.common.error.exception.BadRequestException;
 import com.zootopia.letterservice.common.error.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice // 모든 컨트롤러에서 발생하는 예외를 한 곳에서 처리하게 해주는 어노테이션
 public class GlobalAdviceController {
     // 비즈니스 오류
-    @ExceptionHandler(value = {LetterBadRequestException.class})
-    protected ResponseEntity<ErrorResponse> handleBadRequestException(LetterBadRequestException e) {
+    @ExceptionHandler(value = {BadRequestException.class})
+    protected ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
         ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getHttpStatus().toString(), e.getErrorCode().getErrorCode(), e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(errorResponse);
     }
