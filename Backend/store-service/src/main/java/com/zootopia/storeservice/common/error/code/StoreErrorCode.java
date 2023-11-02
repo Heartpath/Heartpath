5d31d8530986d37b5581dfc4ceb81f9127f8805f
HYEJIN <vvloveletter@naver.com>
2023-11-02 13:11:54 +0900

    🏗 chore :letterservice & storeservice 분리

---
+++ Backend/store-service/src/main/java/com/zootopia/storeservice/common/error/code/StoreErrorCode.java
@@ -0,0 +1,28 @@
+package com.zootopia.storeservice.common.error.code;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum StoreErrorCode {
+    // LetterPaper
+    NOT_EXISTS_LETTERPAPER(HttpStatus.BAD_REQUEST, "LP-001", "존재하지 않는 편지지 입니다."),
+
+    // CrowTit
+    NOT_EXISTS_CROWTIT(HttpStatus.BAD_REQUEST, "CT-001", "존재하지 않는 뱁새입니다."),
+    FAIL_SEND_TO_MEMBER_CROWTIT(HttpStatus.INTERNAL_SERVER_ERROR, "CT-002", "멤버서버로 뱁새 정보 전송에 실패하였습니다."),
+
+    // s3 임시
+    FAIL(HttpStatus.BAD_REQUEST, "S-001", "뷁 실패!");
+
+    private HttpStatus httpStatus;
+    private String errorCode;
+    private String message;
+
+    StoreErrorCode(HttpStatus httpStatus, String errorCode, String message) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
---
+++ Backend/store-service/src/test/java/com/zootopia/storeservice/StoreserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zootopia.storeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
