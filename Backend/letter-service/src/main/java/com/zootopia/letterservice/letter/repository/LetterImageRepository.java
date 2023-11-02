b5c6ad6b43933d47b49870a0887344dc1571fd12
YEONJI <kkgg54@naver.com>
2023-11-02 14:07:40 +0900

    🛠 fix : merge 충돌 해결

--- Backend/letter-service/src/main/java/com/zootopia/letterservice/letter/repository/LetterImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zootopia.letterservice.letter.repository;
-
-import com.zootopia.letterservice.letter.entity.LetterImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LetterImageRepository extends JpaRepository<LetterImage, Long> {
-}
--- Backend/letter-service/src/main/java/com/zootopia/letterservice/letter/repository/PlaceImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zootopia.letterservice.letter.repository;
-
-import com.zootopia.letterservice.letter.entity.PlaceImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceImageRepository extends JpaRepository<PlaceImage, Long> {
-}
