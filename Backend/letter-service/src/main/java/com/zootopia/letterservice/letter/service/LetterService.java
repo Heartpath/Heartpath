674119eb09f74987b917dcb98fe0f2a621ed86aa
YEONJI <kkgg54@naver.com>
2023-11-06 10:44:09 +0900

    ✏ modify : 편지 수기, 텍스트 생성 입력 dto 변경

--- Backend/letter-service/src/main/java/com/zootopia/letterservice/letter/service/LetterService.java
+++ Backend/letter-service/src/main/java/com/zootopia/letterservice/letter/service/LetterService.java
@@ -19,10 +19,9 @@ public interface LetterService {
 
     // Member 추가해야 함.
 //    void createHandLetter (LetterHandReqDto letterHandReqDto, MultipartFile content, List<MultipartFile> files);
-    void createHandLetter (MultipartFile content, List<MultipartFile> files);
+    void createHandLetter (LetterHandReqDto letterHandReqDto, MultipartFile content, List<MultipartFile> files);
 
-//    void createTextLetter (LetterTextReqDto letterTextReqDto, MultipartFile content, List<MultipartFile> files);
-    void createTextLetter (String text, MultipartFile content, List<MultipartFile> files);
+    void createTextLetter (LetterTextReqDto letterTextReqDto, MultipartFile content, List<MultipartFile> files);
 
     void placeLetter(LetterPlaceReqDto letterPlaceReqDto, List<MultipartFile> files);
 
