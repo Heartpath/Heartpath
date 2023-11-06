c6ead294608bca11a97c1e9a89b799208806aa08
김민수 <mskim.spring@gmail.com>
2023-11-06 16:51:46 +0900

    ✨ feat : create User Domain

--- Backend/user-service/src/main/java/com/zootopia/userservice/domain/User.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/domain/User.java
@@ -1,26 +1,41 @@
 package com.zootopia.userservice.domain;
 
 import lombok.Getter;
+import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 
+@Entity
 @Getter
+@ToString
 public class User {
 
+    @Id
+    @Column(name = "MEMBER_ID")
     private String memberID;
 
-    private String kakaoID;
+    @Column(name = "KAKAO_ID")
+    private Long kakaoID;
 
+    @Column(name = "FCM_TOKEN")
     private String fcmToken;
 
+    @Column(name = "NICKNAME")
     private String nickname;
 
+    @Column(name = "POINT")
     private int point;
 
+    @Column(name = "PROFILE_IMAGE_PATH")
     private String profileImagePath;
 
-    private LocalDateTime createdDate;
+    @Column(name = "STATUS")
+    private boolean status;
 
-    private int status;
+    @Column(name = "CREATED_DATE")
+    private LocalDateTime createdDate;
 }
