b1f7adca4e07695a90e1512821485a4fb2426f2f
김민수 <mskim.spring@gmail.com>
2023-11-07 10:19:46 +0900

    ✨ feat : create AccessToken & RefreshToken

--- Backend/user-service/src/main/java/com/zootopia/userservice/jwt/JwtProvider.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/jwt/JwtProvider.java
@@ -1,12 +1,19 @@
 package com.zootopia.userservice.jwt;
 
+import com.zootopia.userservice.util.DateUtil;
+import com.zootopia.userservice.util.JwtUtil;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.security.Key;
+import java.util.Date;
 
 
 @Slf4j
@@ -15,8 +22,58 @@ public class JwtProvider {
 
     private final Key secretKey;
 
-    public JwtProvider(@Value("${jwt.secret-key}") String secretKey) {
+    private final int jwtExpiration;
+    private final int refreshExpiration;
+
+    @Autowired
+    public JwtProvider(@Value("${jwt.secret-key}") String secretKey,
+                       @Value("${jwt.expiration}") int jwtExpiration,
+                       @Value("${jwt.refresh-token.expiration}") int refreshExpiration
+    ) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+
+        this.jwtExpiration = jwtExpiration;
+        this.refreshExpiration = refreshExpiration;
+    }
+
+    private Date ONE_DAY;
+    private Date SEVEN_DAYS;
+
+    @PostConstruct
+    public void calculateDate() {
+        long durationOfOneDay = DateUtil.toDays(jwtExpiration);
+        ONE_DAY = DateUtil.toDate(durationOfOneDay);
+
+        long durationOfSevenDays = DateUtil.toDays(refreshExpiration);
+        SEVEN_DAYS = DateUtil.toDate(durationOfSevenDays);
+    }
+
+    public String createAccessToken(String memberID) {
+
+        String accessToken = Jwts.builder()
+                .setHeaderParams(JwtUtil.getHeaderMap())
+                .setIssuedAt(DateUtil.getCurrentDate())
+                .setExpiration(ONE_DAY)
+                .claim("email", memberID)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+        log.info("Create AccessToken: {}", accessToken);
+
+        return accessToken;
+    }
+
+    public String createRefreshToken(String memberID) {
+
+        String refreshToken = Jwts.builder()
+                .setHeaderParams(JwtUtil.getHeaderMap())
+                .setIssuedAt(DateUtil.getCurrentDate())
+                .setExpiration(SEVEN_DAYS)
+                .claim("email", memberID)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+        log.info("Create RefreshToken: {}", refreshToken);
+
+        return refreshToken;
     }
 }
