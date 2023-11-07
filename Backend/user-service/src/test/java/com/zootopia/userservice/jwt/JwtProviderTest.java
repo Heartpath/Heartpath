4000bd37131491c120c536fa46731182a54a00a2
김민수 <mskim.spring@gmail.com>
2023-11-07 10:45:40 +0900

    💯 test : validate Token

--- Backend/user-service/src/test/java/com/zootopia/userservice/jwt/JwtProviderTest.java
+++ Backend/user-service/src/test/java/com/zootopia/userservice/jwt/JwtProviderTest.java
@@ -1,5 +1,7 @@
 package com.zootopia.userservice.jwt;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@Slf4j
 @SpringBootTest
 class JwtProviderTest {
 
@@ -25,4 +28,28 @@ class JwtProviderTest {
         String refreshToken = jwtProvider.createRefreshToken(memberID);
         System.out.println("refreshToken = " + refreshToken);
     }
+
+    @Test
+    void validateToken() {
+
+        String accessToken = jwtProvider.createAccessToken(memberID);
+
+        boolean res = jwtProvider.validateToken(accessToken);
+        Assertions.assertTrue(res);
+    }
+
+//    @Test
+//    void validateExpiredToken() {
+//
+//        String accessToken = jwtProvider.createAccessToken(memberID);
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            System.out.println("error = " + e);
+//        }
+//
+//        boolean res = jwtProvider.validateToken(accessToken);
+//        Assertions.assertFalse(res);
+//    }
 }
