93a05ba108e4f357fd8e9a95cb265e0c7cd2a282
황신운 <brandy.hwang@gmail.com>
2023-11-07 14:51:08 +0900

    ✏ modify : 회원 가입 부분 수정

--- Mobile/HeartPath/data/src/main/java/com/zootopia/data/datasource/local/PreferenceDataSource.kt
+++ Mobile/HeartPath/data/src/main/java/com/zootopia/data/datasource/local/PreferenceDataSource.kt
@@ -23,15 +23,16 @@ private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
 
 class PreferenceDataSource @Inject constructor(
     @ApplicationContext private val context: Context, // Hilt를 사용하면서 앱 Context를 참고해야할 경우 사용해야함...
-){
+) {
     private object PreferenceKeys {
         val PERMISSION_REJECTED = intPreferencesKey("permission_rejected")
         val BGM_STATE = booleanPreferencesKey("bgm_state")  // BGM 상태
         val FCM_TOKEN = stringPreferencesKey("fcm_token")   // FCM 토큰값
         val ACCESS_TOKEN = stringPreferencesKey("access_token") // access token
         val REFRESH_TOKEN = stringPreferencesKey("refresh_token")   // refresh_token
+        val KAKAO_ACCESS_TOKEN = stringPreferencesKey("kakao_access_token")     // 카카오 access token
     }
-    
+
     fun getPermissionRejected(key: String): Flow<Int> {
         val rejectedPreferencesFlow: Flow<Int> = context.dataStore.data
             .catch { exception ->
@@ -44,10 +45,10 @@ class PreferenceDataSource @Inject constructor(
             .map { preferences ->
                 preferences[intPreferencesKey(key)] ?: 0
             }
-        
+
         return rejectedPreferencesFlow
     }
-    
+
     suspend fun setPermissionRejected(key: String, stack: Int) {
         context.dataStore.edit { preferences ->
             preferences[intPreferencesKey(key)] = stack + 1
@@ -79,7 +80,7 @@ class PreferenceDataSource @Inject constructor(
     }
 
     // FCM token 값 호출
-    fun getFcmToken() : Flow<String> {
+    fun getFcmToken(): Flow<String> {
         val fcmTokenFlow: Flow<String> = context.dataStore.data
             .catch { exception ->
                 // IOException이 발생하는 경우도 있기 때문에 throw-catch 처리
@@ -94,15 +95,16 @@ class PreferenceDataSource @Inject constructor(
             }
         return fcmTokenFlow
     }
+
     // FCM token 값 수정
     suspend fun setFcmToken(token: String) {
-        context.dataStore.edit {preferences ->
+        context.dataStore.edit { preferences ->
             preferences[stringPreferencesKey("fcm_token")] = token
         }
     }
 
     // access token 값 호출
-    fun getAccessToken() : Flow<String> {
+    fun getAccessToken(): Flow<String> {
         return context.dataStore.data
             .catch { exception ->
                 // IOException이 발생하는 경우도 있기 때문에 throw-catch 처리
@@ -119,10 +121,11 @@ class PreferenceDataSource @Inject constructor(
 
     // access token 값 수정
     suspend fun setAccessToken(accessToken: String) {
-        context.dataStore.edit {preferences ->
+        context.dataStore.edit { preferences ->
             preferences[stringPreferencesKey("access_token")] = accessToken
         }
     }
+
     // refresh token 값 호출
     fun getRefreshToken(): Flow<String> {
         return context.dataStore.data
@@ -141,8 +144,30 @@ class PreferenceDataSource @Inject constructor(
 
     // refresh token 값 수정
     suspend fun setRefreshToken(refreshToken: String) {
-        context.dataStore.edit {preferences ->
+        context.dataStore.edit { preferences ->
             preferences[stringPreferencesKey("refresh_token")] = refreshToken
+        }
+    }
+
+    // kakao access token 값 호출
+    fun getKakaoAccessToken(): Flow<String> {
+        return context.dataStore.data
+            .catch { exception ->
+                // IOException이 발생하는 경우도 있기 때문에 throw-catch 처리
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                preferences[stringPreferencesKey("kakao_access_token")] ?: ""
+            }
+    }
+    // kakao access token 값 수정
+    suspend fun setKakaoAccessToken(accessToken: String) {
+        context.dataStore.edit {preferences ->
+            preferences[stringPreferencesKey("kakao_access_token")] = accessToken
         }
     }
 }
--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/login/LoginViewModel.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/login/LoginViewModel.kt
@@ -2,18 +2,32 @@ package com.zootopia.presentation.login
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.zootopia.presentation.config.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 private const val TAG = "LoginViewModel_HP"
 @HiltViewModel
 class LoginViewModel @Inject constructor() :
 BaseViewModel() {
+
+    private val _kakaoAccessToken = MutableStateFlow("")
+    var kakaoAccessToken = _kakaoAccessToken.asStateFlow()
+
+    private val _newId = MutableStateFlow("")
+    var newId = _newId.asStateFlow()
+
+    private val _checkIdDone = MutableStateFlow(false)
+    var checkIdDone = _checkIdDone.asStateFlow()
+
     fun loginByKakao(context: Context) {
         val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             Log.d(TAG, "loginByKakao: here is callback")
@@ -21,7 +35,11 @@ BaseViewModel() {
                 Log.d(TAG, "로그인 실패 $error")
             } else if (token != null) {
                 Log.d(TAG, "로그인 성공 ${token.accessToken}")
-                Log.d(TAG, "loginByKakaoCallback: ${token.refreshToken}")
+                viewModelScope.launch {
+                    // 카카오 access token 값 넣어주기
+                    _kakaoAccessToken.emit(token.accessToken)
+                    login()
+                }
             }
         }
         // 카카오톡 설치 확인
@@ -43,15 +61,47 @@ BaseViewModel() {
                 // 로그인 성공 부분
                 else if (token != null) {
                     Log.d(TAG, "로그인 성공 ${token.accessToken}")
-                    Log.d(TAG, "loginByKakao: ${token.refreshToken}")
-                    Log.d(TAG, "loginByKakao: ${token.idToken}")
-                    Log.d(TAG, "loginByKakao: ${token.scopes.toString()}")
+                    viewModelScope.launch {
+                        // 카카오 access token 값 넣어주기
+                        _kakaoAccessToken.emit(token.accessToken)
+                        login()
+                    }
                 }
             }
         } else {
             UserApiClient.instance.loginWithKakaoAccount(context = context, callback = kakaoCallback) // 카카오 이메일 로그인
         }
     }
+
+
+    // 로그인 - 카카오 access token 넣어서 전송
+    fun login() = viewModelScope.launch {
+        // TODO: 카카오 access token 넣어서 서버에 로그인 요청
+
+    }
+
+    // id 입력 값 수정
+    fun setNewId(value: String) = viewModelScope.launch {
+        _newId.emit(value)
+        Log.d(TAG, "setNewId: ${newId.value}")
+    }
+
+    // 아이디 중복 체크
+    fun duplicateCheckId() = viewModelScope.launch {
+        // 만약에 중복 되지 않았으면 true 값으로 변경
+
+    }
+
+    fun setCheckIdDone(value: Boolean) = viewModelScope.launch {
+        _checkIdDone.emit(value)
+        Log.d(TAG, "setCheckIdDone: ${checkIdDone.value}")
+    }
+
+    // 회원가입 요청
+    fun signUp() = viewModelScope.launch {
+
+    }
+
 
 
 
