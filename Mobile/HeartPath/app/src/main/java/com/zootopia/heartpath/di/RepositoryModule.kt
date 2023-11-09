14e5c6ce2ed74961b64ca8f96b347e4cb8352517
ShinWoon <brandy.hwang@gmail.com>
2023-11-09 09:24:01 +0900

    ✨ feat : 마이페이지에 내 정보 띄우기 호출 userInfo 통신 구현 userInfo 접근 200 뜨는거 확인, access token이 header에 들어가는 것이 확인, 서버 값이 올바르게 나오지 않아 추후 다시 테스트 필요

--- Mobile/HeartPath/app/src/main/java/com/zootopia/heartpath/di/RepositoryModule.kt
+++ Mobile/HeartPath/app/src/main/java/com/zootopia/heartpath/di/RepositoryModule.kt
@@ -3,12 +3,16 @@ package com.zootopia.heartpath.di
 import com.zootopia.data.datasource.local.PreferenceDataSource
 import com.zootopia.data.datasource.remote.login.LoginDataSource
 import com.zootopia.data.datasource.remote.map.MapDataSource
+import com.zootopia.data.datasource.remote.user.UserDataSource
+import com.zootopia.data.datasource.remote.user.UserDataSourceImpl
 import com.zootopia.data.repository.PreferenceRepositoryImpl
 import com.zootopia.data.repository.login.LoginRepositoryImpl
 import com.zootopia.data.repository.map.MapRepositoryImpl
+import com.zootopia.data.repository.user.UserRepositoryImpl
 import com.zootopia.domain.repository.PreferenceRepository
 import com.zootopia.domain.repository.login.LoginRepository
 import com.zootopia.domain.repository.map.MapRepository
+import com.zootopia.domain.repository.user.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,5 +39,11 @@ object RepositoryModule {
     @Provides
     fun provideLoginRepository(loginDataSource: LoginDataSource, preferenceDataSource: PreferenceDataSource): LoginRepository {
         return LoginRepositoryImpl(loginDataSource = loginDataSource, preferenceDataSource = preferenceDataSource)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(userDataSource: UserDataSource): UserRepository {
+        return UserRepositoryImpl(userDataSource = userDataSource)
     }
 }
---
+++ Mobile/HeartPath/data/src/main/java/com/zootopia/data/repository/user/UserRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.zootopia.data.repository.user
+
+import com.zootopia.data.datasource.remote.user.UserDataSource
+import com.zootopia.data.mapper.toDomain
+import com.zootopia.domain.model.user.UserInfoDto
+import com.zootopia.domain.repository.user.UserRepository
+import com.zootopia.domain.util.getValueOrThrow2
+
+class UserRepositoryImpl(
+    private val userDataSource: UserDataSource,
+): UserRepository {
+    override suspend fun getUserInfo(): UserInfoDto {
+        return getValueOrThrow2 {
+            userDataSource.getUserInfo().toDomain()
+        }
+    }
+
+}
