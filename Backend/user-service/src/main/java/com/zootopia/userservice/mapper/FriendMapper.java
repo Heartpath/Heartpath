ce4f6a421b57a9c4affe7f1bfeb821fcca9b9640
김민수 <mskim.spring@gmail.com>
2023-11-09 10:51:08 +0900

    ✨ feat : 친구 목록 불러오기

--- Backend/user-service/src/main/java/com/zootopia/userservice/mapper/FriendMapper.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/mapper/FriendMapper.java
@@ -1,5 +1,6 @@
 package com.zootopia.userservice.mapper;
 
+import com.zootopia.userservice.dto.FriendInfoDTO;
 import com.zootopia.userservice.dto.FriendShipDTO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,4 +11,6 @@ import java.util.List;
 public interface FriendMapper {
 
     List<FriendShipDTO> getRelationshipWithFriends(String from, String to);
+
+    List<FriendInfoDTO> getFriendInfoList(String memberID);
 }
