f0d09292ebdbd0c3f72979f71b615918b82c6749
Ha DongHyeok <mimiga1@naver.com>
2023-11-10 01:08:25 +0900

    ✨ feat : 위치서비스 에러 수정 & AR 버튼으로 편지 읽기로 변경 | 하동혁
    - AR: 편지 담기 버튼클릭시 이벤트 처리해야함
    - 편지를 성공적으로 담으면 -> 백그라운드 종료
    - 편지를 담기 하지 않고 뒤로 오면 -> 지도 경로 다시 그리고, 백그라운드 계속 실행, 카메라 위치 (내 위치와 목적지 기반으로 다시 세팅)

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/map/MapViewModel.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/map/MapViewModel.kt
@@ -26,29 +26,30 @@ class MapViewModel @Inject constructor(
     private val requestTmapWalkRoadUseCase: RequestTmapWalkRoadUseCase,
     private val testUseCase: testUseCase,
 ) : BaseViewModel() {
-    
+
     // 편지 신고 삭제 신고 클릭 유무
     var isReport = false
-    
+
     // 더미더미더미
     var LetterList = mutableListOf<MapLetterDto>(
-        MapLetterDto(1,false, "하동혁님이 보낸 편지1", "2023.11.01", "36.1094880", "128.420560"),
-        MapLetterDto(2,false, "하동혁님이 보낸 편지2", "2023.11.01","36.1051004", "128.422769"),
-        MapLetterDto(3,false, "하동혁님이 보낸 편지3", "2023.11.01","36.111282119957956", "128.42349987855764 "),
+        MapLetterDto(1, false, "하동혁님이 보낸 편지1", "2023.11.01", "36.1094880", "128.420560"),
+        MapLetterDto(2, false, "하동혁님이 보낸 편지2", "2023.11.01", "36.1051004", "128.422769"),
+        MapLetterDto(3, false, "하동혁님이 보낸 편지3", "2023.11.01", "36.111282119957956", "128.42349987855764"),
+        MapLetterDto(4, false, "우리집", "2023.11.01", "36.121949856660855", "128.38053050168602"),
     )
-    
+
     private val _mapLetterList = MutableSharedFlow<MutableList<MapLetterDto>>()
     val mapLetterList: SharedFlow<MutableList<MapLetterDto>>
         get() = _mapLetterList
-    
+
     fun getDummyList() {
         viewModelScope.launch {
             _mapLetterList.emit(LetterList)
         }
     }
-    
+
     // user posi
-    var lastLatitude:Double = 0.0
+    var lastLatitude: Double = 0.0
     var lastLongitude: Double = 0.0
     val lastLocation = Location("userProvider")
     fun setLocation(latitude: Double, longitude: Double) {
@@ -59,37 +60,36 @@ class MapViewModel @Inject constructor(
         lastLocation.latitude = lastLatitude
         lastLocation.longitude = lastLongitude
     }
-    
-    
+
     // marker posi
     var dist: String = "--m"
     var goalLatitude: Double = 0.0
     var goalLongitude: Double = 0.0
     val goalLocation = Location("goalProvider")
-    
+
     fun makeGoalLocataion() {
         goalLocation.latitude = goalLatitude
         goalLocation.longitude = goalLongitude
     }
-    
+
     // 길찾기 상태
     var isStartWalk = false
     private val _isWorkManager = MutableSharedFlow<Boolean>()
     val isWorkManager: SharedFlow<Boolean>
         get() = _isWorkManager
-    
+
     // Tmap
     private val _tmapWalkRoadInfo = MutableSharedFlow<FeatureCollectionDto>()
     val tmapWalkRoadInfo: SharedFlow<FeatureCollectionDto>
         get() = _tmapWalkRoadInfo.asSharedFlow()
-    
+
     @OptIn(ExperimentalCoroutinesApi::class)
     fun resetTmapWalkRoadInfo() {
         _tmapWalkRoadInfo.resetReplayCache()
     }
-    
+
     fun requestTmapWalkRoad(
-        mapLetterDto: MapLetterDto
+        mapLetterDto: MapLetterDto,
     ) {
         getApiResult(
             block = {
@@ -103,33 +103,29 @@ class MapViewModel @Inject constructor(
                         reqCoordType = "WGS84GEO", // 위경도 표현 타입 코드
                         resCoordType = "WGS84GEO",
                         startName = "내 위치",
-                        endName = "편지"
-                    )
+                        endName = "편지",
+                    ),
                 )
             },
             success = { result ->
                 Log.d(TAG, "requestTmapWalkRoad: ${result.features}")
                 _tmapWalkRoadInfo.emit(result)
-            }
+            },
         )
     }
     // Tmap - END
-    
-    
+
     private val _walkDistance = MutableSharedFlow<Double>()
     val walkDistance: SharedFlow<Double>
         get() = _walkDistance.asSharedFlow()
-    
+
     fun calculateDistance() {
         val dist = lastLocation.distanceTo(goalLocation)
         viewModelScope.launch {
             _walkDistance.emit(dist.toDouble())
         }
     }
-    
 
-    
-    
     // 임시 사용
     fun test() {
         getApiResult(
@@ -138,8 +134,7 @@ class MapViewModel @Inject constructor(
             },
             success = {
                 Log.d(TAG, "test: $it")
-            }
+            },
         )
     }
-    
 }
