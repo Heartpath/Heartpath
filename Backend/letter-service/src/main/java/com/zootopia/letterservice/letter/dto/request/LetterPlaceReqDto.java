d8f2e7e31ca2be7e80fd0dbd4c13b2c9ed1fb00b
YEONJI <kkgg54@naver.com>
2023-11-09 15:45:30 +0900

    ✏ modify : 편지 배치시 필요한 dto 수정

--- Backend/letter-service/src/main/java/com/zootopia/letterservice/letter/dto/request/LetterPlaceReqDto.java
+++ Backend/letter-service/src/main/java/com/zootopia/letterservice/letter/dto/request/LetterPlaceReqDto.java
@@ -11,8 +11,6 @@ import java.util.List;
 @NoArgsConstructor
 public class LetterPlaceReqDto {
     private String id;
-    private String senderId;
-    private String receiverId;
     private Double lat;
     private Double lng;
 
