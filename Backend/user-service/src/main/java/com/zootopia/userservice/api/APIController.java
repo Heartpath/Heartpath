85175e1bca860e524df1346d34f93ef98d1bacf6
김민수 <mskim.spring@gmail.com>
2023-11-09 15:16:46 +0900

    📃 docs : ApiController Swagger 문서화

--- Backend/user-service/src/main/java/com/zootopia/userservice/api/APIController.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/api/APIController.java
@@ -7,6 +7,10 @@ import com.zootopia.userservice.exception.FriendException;
 import com.zootopia.userservice.jwt.JwtProvider;
 import com.zootopia.userservice.service.APIService;
 import com.zootopia.userservice.util.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +22,9 @@ import java.util.HashMap;
 import java.util.List;
 
 
-@Tag(name = "회원 관련 정보 조회 API", description = "모든 요청은 AccessToken이 필요합니다. Header에 담아서 보내주세요. \n Authorization: ")
+@Tag(name = "회원 관련 정보 조회 API",
+        description = "서버간 통신에서만 이용 가능합니다. <b>Android에서 호출하면 4xx Error 발생합니다.</b>"
+)
 @Slf4j
 @RestController
 @RequestMapping("/api")
@@ -28,6 +34,25 @@ public class APIController {
     private final APIService apiService;
     private final JwtProvider jwtProvider;
 
+    @Operation(summary = "JWT로 멤버 정보 반환")
+    @ApiResponse(
+            responseCode = "200", description = "JWT 토큰이 필요합니다.",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n" +
+                            "    \"status\": 200,\n" +
+                            "    \"message\": \"ㅇㅇ의 회원 정보입니다.\",\n" +
+                            "    \"data\": {\n" +
+                            "        \"memberID\": \"\",\n" +
+                            "        \"fcmToken\": \"\",\n" +
+                            "        \"nickname\": \"\",\n" +
+                            "        \"profileImagePath\": \"\",\n" +
+                            "        \"point\": \"\",\n" +
+                            "        \"createdDateTime\": \"\"\n" +
+                            "    }\n" +
+                            "}")
+            )
+    )
     @GetMapping("/user")
     public ResponseEntity<BaseResponse> getMemberInfoByJWT(HttpServletRequest request) {
 
@@ -42,6 +67,21 @@ public class APIController {
         return ResponseEntity.status(200).body(baseResponse);
     }
 
+    @Operation(summary = "멤버 ID로 FCM 토큰, 닉네임 반환")
+    @ApiResponse(
+            responseCode = "200", description = "JWT 토큰이 필요없습니다.",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n" +
+                            "    \"status\": 200,\n" +
+                            "    \"message\": \"FCM 토큰, 닉네임 반환되었습니다.\",\n" +
+                            "    \"data\": {\n" +
+                            "        \"nickname\": \"\",\n" +
+                            "        \"fcmToken\": \"\"\n" +
+                            "    }\n" +
+                            "}")
+            )
+    )
     @GetMapping("/user/{memberID}")
     public ResponseEntity<BaseResponse> getMemberInfoByMemberID(@PathVariable String memberID) {
 
@@ -66,6 +106,25 @@ public class APIController {
         return ResponseEntity.status(200).body(baseResponse);
     }
 
+    @Operation(summary = "멤버 A와 멤버 B의 친구 관계 정보")
+    @ApiResponse(
+            responseCode = "200", description = "JWT 토큰이 필요합니다.",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "[\n" +
+                            "        {\n" +
+                            "            \"from\": \"ssafyA\",\n" +
+                            "            \"to\": \"ssafyB\",\n" +
+                            "            \"blocked\": false\n" +
+                            "        },\n" +
+                            "        {\n" +
+                            "            \"from\": \"ssafyB\",\n" +
+                            "            \"to\": \"ssafyA\",\n" +
+                            "            \"blocked\": false\n" +
+                            "        }\n" +
+                            "    ]")
+            )
+    )
     @PostMapping("/friend")
     public ResponseEntity<BaseResponse> verifyFriendShip(@RequestBody FriendShipDTO friendShipDTO) {
 
@@ -74,15 +133,24 @@ public class APIController {
         String to = friendShipDTO.getTo();
 
         List<FriendShipDTO> res = apiService.checkRelationshipWithFriends(from, to);
+        String desc = String.format("%s과 %s의 친구 관계 정보입니다.", from, to);
 
         BaseResponse baseResponse = new BaseResponse(
                 200,
-                "회원 정보",
+                desc,
                 res);
 
         return ResponseEntity.status(200).body(baseResponse);
     }
 
+    @Operation(summary = "멤버 ID로 임시 JWT 토큰 반환")
+    @ApiResponse(
+            responseCode = "200", description = "JWT 토큰이 필요없습니다.",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "JWT 토큰")
+            )
+    )
     @GetMapping("/token/{memberID}")
     public String getTmpToken(@PathVariable String memberID) {
         return jwtProvider.createAccessToken(memberID);
