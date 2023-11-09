8c6f58a38e11dbde061f7897f281d586ecb81c9c
ShinWoon <brandy.hwang@gmail.com>
2023-11-10 02:46:43 +0900

    ✨ feat : point api 연결 완료

--- Mobile/HeartPath/data/src/main/java/com/zootopia/data/mapper/UserMapper.kt
+++ Mobile/HeartPath/data/src/main/java/com/zootopia/data/mapper/UserMapper.kt
@@ -1,10 +1,14 @@
 package com.zootopia.data.mapper
 
+import com.zootopia.data.model.user.response.PointInfoResponse
 import com.zootopia.data.model.user.response.UserInfoResponse
+import com.zootopia.domain.model.user.PointDto
 import com.zootopia.domain.model.user.UserInfoDto
 
 fun UserInfoResponse.toDomain(): UserInfoDto? {
-    return UserInfoDto(
-        return data
-    )
+    return data
+}
+
+fun PointInfoResponse.toDomain(): List<PointDto>? {
+    return data
 }
