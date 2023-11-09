fd5cb96d2c9eed227ef97b8b846a3420c346dc68
김민수 <mskim.spring@gmail.com>
2023-11-09 15:16:46 +0900

    📃 docs : ApiController Swagger 문서화

--- Backend/user-service/src/main/java/com/zootopia/userservice/filter/VerifyJwtFilter.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/filter/VerifyJwtFilter.java
@@ -33,7 +33,7 @@ public class VerifyJwtFilter extends OncePerRequestFilter {
     // TODO: yaml 파일로 빼기
     private static final String[] EXCLUDED_URLS = {"/user/health_check",
             "/user/login", "/user/register", "/user/check",
-            "/api/user", "/api/token",
+            "/api/user/", "/api/token",
             "/swagger-ui", "/v3/api-docs"};
 
     private String extractJwtFromHeader(Optional<String> authorizationToken) throws JwtException {
