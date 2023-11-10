7b8a4eaac5e03091ec8fce88734d406e713b421b
김민수 <mskim.spring@gmail.com>
2023-11-10 12:59:49 +0900

    ✏ modify : disable PointController

--- Backend/user-service/src/main/java/com/zootopia/userservice/controller/PointController.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/controller/PointController.java
@@ -1,38 +1,38 @@
-package com.zootopia.userservice.controller;
-
-import com.zootopia.userservice.common.BaseResponse;
-import com.zootopia.userservice.dto.UserPointTXDTO;
-import com.zootopia.userservice.service.PointService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-
-@Slf4j
-@RestController
-@RequestMapping("/user")
-@RequiredArgsConstructor
-public class PointController {
-
-    private final PointService pointService;
-
-    private String memberID;
-
-    public void setMemberID(String memberID) {
-        this.memberID = memberID;
-    }
-
-    @GetMapping("/point")
-    public ResponseEntity<BaseResponse> getUserPointTransaction() {
-
-        List<UserPointTXDTO> userPointTXDTOS = pointService.loadUserPointTransaction(memberID);
-        BaseResponse baseResponse = new BaseResponse(200, "유저 포인트 사용 내역 조회", userPointTXDTOS);
-
-        return ResponseEntity.status(200).body(baseResponse);
-    }
-}
+//package com.zootopia.userservice.controller;
+//
+//import com.zootopia.userservice.common.BaseResponse;
+//import com.zootopia.userservice.dto.UserPointTXDTO;
+//import com.zootopia.userservice.service.PointService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/user")
+//@RequiredArgsConstructor
+//public class PointController {
+//
+//    private final PointService pointService;
+//
+//    private String memberID;
+//
+//    public void setMemberID(String memberID) {
+//        this.memberID = memberID;
+//    }
+//
+//    @GetMapping("/point")
+//    public ResponseEntity<BaseResponse> getUserPointTransaction() {
+//
+//        List<UserPointTXDTO> userPointTXDTOS = pointService.loadUserPointTransaction(memberID);
+//        BaseResponse baseResponse = new BaseResponse(200, "유저 포인트 사용 내역 조회", userPointTXDTOS);
+//
+//        return ResponseEntity.status(200).body(baseResponse);
+//    }
+//}
