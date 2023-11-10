c6258105c881d19a61ee7f6affe927ef4a1fde49
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-11 00:03:34 +0900

    ♻ refactor : 편지 구매 전 이므로 편지지 임시 데이터로 수정

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/writeletter/WriteLetterViewModel.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/writeletter/WriteLetterViewModel.kt
@@ -28,9 +28,10 @@ class WriteLetterViewModel @Inject constructor(
     private var _selectedLetterPaperUrl: MutableStateFlow<String> = MutableStateFlow("")
     val selectedLetterPaperUrl: StateFlow<String> = _selectedLetterPaperUrl
 
-    private var _letterPaperList: MutableStateFlow<MutableList<UserLetterPaperDto>> = MutableStateFlow(
-        mutableListOf()
-    )
+    private var _letterPaperList: MutableStateFlow<MutableList<UserLetterPaperDto>> =
+        MutableStateFlow(
+            mutableListOf()
+        )
     val letterPaperList: StateFlow<MutableList<UserLetterPaperDto>> = _letterPaperList
 
     private var _selectedColor: MutableStateFlow<Int> = MutableStateFlow<Int>(R.color.black)
@@ -72,7 +73,7 @@ class WriteLetterViewModel @Inject constructor(
         resetBitmap()
     }
 
-    fun resetBitmap(){
+    fun resetBitmap() {
         _drawingBitmap.value = null
         _penBitmap.value = null
     }
@@ -95,7 +96,7 @@ class WriteLetterViewModel @Inject constructor(
         }
     }
 
-    fun setLetterPaperSize(width: Float, height: Float){
+    fun setLetterPaperSize(width: Float, height: Float) {
         viewModelScope.launch {
             _letterPaperWidth.value = width
             _letterPaperHeight.value = height
@@ -104,29 +105,28 @@ class WriteLetterViewModel @Inject constructor(
 
     fun getLetterPaperList() {
         //서버로 부터 편지지 리스트를 받는다.
-        getApiResult(
-            block = {
-                getUserLetterPaperUseCase.invoke()
-            },
-            success = {
-                _letterPaperList.emit(it)
-            }
-        )
-//        viewModelScope.launch {
-//            var list = mutableListOf<String>().apply {
-//                add("https://postfiles.pstatic.net/MjAyMzEwMjhfNjcg/MDAxNjk4NDI0ODE5NjI4.kPCHa288iH5JCl8arHKkxb-X5vq_zph7A8N7B6YTiJIg.56HHDL7-Jb3xGtjdMpdNnUPltZkV7HVZ0Hhk-AosBBEg.PNG.vmfpel0425/export202310280139235243.png?type=w773")
-//                add("https://postfiles.pstatic.net/MjAyMzEwMjhfNTMg/MDAxNjk4NDIyODU5NzQz.efoLw5ABlo7eg_dVvCB6gxLCZjvuPRMTOlc7Obt4tHMg.wzujXlvx44kykz1JfcHRikbdFCZoBeGckASWcqJjSmMg.PNG.vmfpel0425/export202310280100244537.png?type=w773")
-//                add("https://postfiles.pstatic.net/MjAyMzEwMjhfMTEw/MDAxNjk4NDI4MDk5MzAz.AGlsyuL5RlHfwm4c-n79IfJpDOIScnL9hu8zBIwsJhkg.63uoa5hAWNijsFU8DuSLrZdHqXxnXgsMIM9AlqTz3KAg.PNG.vmfpel0425/export202310280234128271.png?type=w773")
-//                add("https://postfiles.pstatic.net/MjAyMzEwMjhfNDcg/MDAxNjk4NDI5OTUzMjc0.zzPnSz3F2lcumdN_KbXvUB6vDa1qgPYvKZ2H2ntqNGEg._x8G_WEtyChmAvRDqpJi3988M6KdxNjLDgA-i5eBGPQg.PNG.vmfpel0425/export202310280304530889.png?type=w773")
-//                add("https://postfiles.pstatic.net/MjAyMzEwMjhfMjkx/MDAxNjk4NDMxNjA5ODE4.5DV-03qZur91-juxpIlZlHMKwJ1_YCAcAXVWBYk873Mg.keXERyWc6DbWLHhAEcuhfxZtLJEbqeMx7PvKvtYu_Rcg.PNG.vmfpel0425/export202310280332561002.png?type=w773")
-//                add("https://img.freepik.com/free-vector/paper-note-frame-vector-on-memphis-pattern_53876-108786.jpg?w=360&t=st=1698396316~exp=1698396916~hmac=94d23abab6ea4c081de421792add4fd65d41c77270948725a80fae01ad16e994")
-//                add("https://img.freepik.com/free-vector/empty-paper-on-daisy-field-patterned-background_53876-115658.jpg?w=360&t=st=1698396333~exp=1698396933~hmac=fdb3786b47d67f9256107a83d9fe7c4df5988a330311df12e6032eb87e910013")
-//                add("https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/04/22/91193624-51b9-4b28-b3b2-bef73c770691.jpg")
-//                add("https://img.allurekorea.com/allure/2023/08/style_64d0bb505c802-768x1024.jpg")
-//                add("https://img.freepik.com/premium-photo/cute-gray-cat-kid-animal-with-interested-question-facial-face-expression-look-up-on-copy-space-small-tabby-kitten-on-white-background-vertical-format_221542-2278.jpg?w=360")
+//        getApiResult(
+//            block = {
+//                getUserLetterPaperUseCase.invoke()
+//            },
+//            success = {
+//                _letterPaperList.emit(it)
 //            }
-//            _letterPaperList.emit(list)
-//        }
+//        )
+        viewModelScope.launch {
+            var list = mutableListOf<UserLetterPaperDto>().apply {
+                add(
+                    UserLetterPaperDto(
+                        0,
+                        "d",
+                        12,
+                        "https://postfiles.pstatic.net/MjAyMzEwMjhfNjcg/MDAxNjk4NDI0ODE5NjI4.kPCHa288iH5JCl8arHKkxb-X5vq_zph7A8N7B6YTiJIg.56HHDL7-Jb3xGtjdMpdNnUPltZkV7HVZ0Hhk-AosBBEg.PNG.vmfpel0425/export202310280139235243.png?type=w773",
+                        false
+                    )
+                )
+            }
+            _letterPaperList.emit(list)
+        }
     }
 
     fun searchUser(searchKeyWord: String) {
@@ -158,11 +158,11 @@ class WriteLetterViewModel @Inject constructor(
         _drawingBitmap.value = bitmap
     }
 
-    fun setPenBitmap(bitmap: Bitmap){
+    fun setPenBitmap(bitmap: Bitmap) {
         _penBitmap.value = bitmap
     }
 
-    fun addImageList(list: MutableList<Uri>){ //리스트에 이미지를 추가함
+    fun addImageList(list: MutableList<Uri>) { //리스트에 이미지를 추가함
         viewModelScope.launch {
             val newList = mutableListOf<Uri>()
 
@@ -178,7 +178,7 @@ class WriteLetterViewModel @Inject constructor(
         }
     }
 
-    fun setImageList(list: MutableList<Uri>){ //리스트를 새로 갈아줌
+    fun setImageList(list: MutableList<Uri>) { //리스트를 새로 갈아줌
         val newImageList = mutableListOf<Uri>()
         list.forEachIndexed { index, uri ->
             newImageList.add(uri)
@@ -186,11 +186,11 @@ class WriteLetterViewModel @Inject constructor(
         _imageList.value = newImageList
     }
 
-    fun resetImageList(){
+    fun resetImageList() {
         _imageList.value = mutableListOf()
     }
 
-    fun resetIsSendSuccess(){
+    fun resetIsSendSuccess() {
         _isSendSuccess.value = false
     }
 
