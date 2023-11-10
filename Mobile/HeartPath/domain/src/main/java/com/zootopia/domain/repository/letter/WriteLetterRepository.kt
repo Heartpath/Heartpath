cbe066b78f454dea9d1c4f776b4eedde34ab8a59
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-10 17:59:21 +0900

    ♻ refactor : BusinessRepository, BusinessRepositoryImpl의 이름을 WriteLetterRepository, WriteLetterRepositoryImpl로 변경

--- Mobile/HeartPath/domain/src/main/java/com/zootopia/domain/repository/letter/WriteLetterRepository.kt
+++ Mobile/HeartPath/domain/src/main/java/com/zootopia/domain/repository/letter/WriteLetterRepository.kt
@@ -1,8 +1,8 @@
-package com.zootopia.domain.repository.business
+package com.zootopia.domain.repository.letter
 
 import com.zootopia.domain.model.writeletter.HandLetterRequestDto
 
-interface BusinessRepository {
+interface WriteLetterRepository {
     suspend fun postHandWriteLetter(
         handLetterRequestDto: HandLetterRequestDto,
         content: String,
