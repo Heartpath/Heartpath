f8cca826bfd2ff23a440a063be13072a86372ed0
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-11 02:59:45 +0900

    ✨ feat : 타이핑 편지 작성 화면 수정
    -편지지에 맞게 edit text 사이즈 조정
    -edit text에 맞게 최대 라인 수를 font높이로 계산
    -최대 라인 수를 넘으면 입력이 되지 않는 text watcher 추가, 적용

---
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/util/MaxLineLimitTextWatcher.kt
@@ -0,0 +1,28 @@
+package com.zootopia.presentation.util
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+class MaxLineLimitTextWatcher(val limitCount: Int, val editText: EditText) : TextWatcher{
+
+    private var lastValue = ""
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { // 입력 변화 반영 전 처리
+        lastValue = p0.toString()
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { // 변화와 동시 처리
+
+    }
+
+    override fun afterTextChanged(p0: Editable?) { // 입력 후 처리
+        if(editText.lineCount > limitCount){
+            var selectionStart = editText.selectionStart -1
+            editText.setText(lastValue)
+            if(selectionStart >= editText.length()){
+                selectionStart = editText.length()
+            }
+            editText.setSelection(selectionStart)
+        }
+    }
+}
