4e5427f35c6a75f561602b1713a3beb61b68b96f
김민수 <mskim.spring@gmail.com>
2023-11-10 12:35:00 +0900

    ✨ feat : 유저 ID로 유저 검색

---
+++ Backend/user-service/src/main/java/com/zootopia/userservice/dto/UserSearchDTO.java
@@ -0,0 +1,14 @@
+package com.zootopia.userservice.dto;
+
+import lombok.Getter;
+
+
+@Getter
+public class UserSearchDTO {
+
+    private String memberID;
+
+    private String nickname;
+
+    private String profileImagePath;
+}
--- Backend/user-service/src/main/java/com/zootopia/userservice/mapper/UserMapper.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/mapper/UserMapper.java
@@ -1,11 +1,17 @@
 package com.zootopia.userservice.mapper;
 
 import com.zootopia.userservice.dto.MypageDTO;
+import com.zootopia.userservice.dto.QueryParamMap;
+import com.zootopia.userservice.dto.UserSearchDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 
 @Mapper
 public interface UserMapper {
 
     MypageDTO readUserInfo(String memberID);
+
+    List<UserSearchDTO> readLimitUserByID(QueryParamMap paramMap);
 }
