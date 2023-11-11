63e12e4a17b7c107e2dd36f62708acdab51f9add
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-11 22:37:56 +0900

    ♻ refactor : 편지지 url을 받는 로직 수정
    -viewModel에서 url을 가져오는 방법에서 safe args로 url을 받는 방법으로 수정함, 이전 방식에서는 url 을 collect하고 이미지가 올라가는 view가 완성되는것을 listen해야해서 collect되었을때와 view가 올라갔을때 두번 다 이미지 뷰 크기조정, 이미지 올리기를 했으나 텍스트 메시지 작성에서 해당 방법을 사용할때 문제가 있어 방법을 변경함

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/writeletter/typingwrite/TypingWriteFragment.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/writeletter/typingwrite/TypingWriteFragment.kt
@@ -8,12 +8,14 @@ import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
+import android.widget.Toast
 import androidx.compose.ui.geometry.Rect
 import androidx.core.view.marginTop
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
@@ -25,7 +27,9 @@ import com.zootopia.presentation.util.LetterType
 import com.zootopia.presentation.util.MaxLineLimitTextWatcher
 import com.zootopia.presentation.util.viewToBitmap
 import com.zootopia.presentation.writeletter.WriteLetterViewModel
+import com.zootopia.presentation.writeletter.selectletterpaper.SelectLetterPaperFragmentArgs
 import kotlinx.coroutines.launch
+import kotlin.math.log
 
 private const val TAG = "TypingWriteFragment"
 class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
@@ -37,6 +41,8 @@ class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
     private val writeLetterViewModel: WriteLetterViewModel by activityViewModels()
     private var imageViewHeight: Float = 0F
     private var imageViewWidth: Float = 0F
+    private val args: TypingWriteFragmentArgs by navArgs()
+    private var url = ""
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -46,7 +52,7 @@ class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-
+        url = args.letterUrl
         initViewReadyListener()
         initCollecter()
         initListener()
@@ -63,23 +69,24 @@ class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
                         .removeOnGlobalLayoutListener(this)
                     imageViewHeight = binding.imageviewLetterPaper.getHeight().toFloat()
                     imageViewWidth = binding.imageviewLetterPaper.getWidth().toFloat()
-                    setLetterPaper(writeLetterViewModel.selectedLetterPaperUrl.value)
+                    Log.d(TAG, "onGlobalLayout: 왜안됨 ${imageViewHeight} ${imageViewWidth}")
+                    setLetterPaper()
                 }
             })
         }
     }
 
     private fun initCollecter() = with(binding) {
-        lifecycleScope.launch {
-            writeLetterViewModel.selectedLetterPaperUrl.collect {
-                Log.d(TAG, "initCollecter: url collect")
-                setLetterPaper(it)
-            }
-        }
+//        lifecycleScope.launch {
+//            writeLetterViewModel.selectedLetterPaperUrl.collect {
+//                Log.d(TAG, "initCollecter: url collect")
+//                setLetterPaper(it)
+//            }
+//        }
 
     }
 
-    private fun setLetterPaper(url: String) = with(binding) {
+    private fun setLetterPaper() = with(binding) {
         Glide.with(mainActivity).load(url)
             .into(object : CustomTarget<Drawable>() {
                 override fun onResourceReady(
@@ -89,10 +96,11 @@ class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
                     // 이미지의 너비와 높이를 가져옴
                     val imageWidth = resource.intrinsicWidth.toFloat()
                     val imageHeight = resource.intrinsicHeight.toFloat()
-
+                    Log.d(TAG, "onResourceReady: 1111 ${imageWidth} ${imageHeight}")
                     // 이미지의 비율을 계산
                     val imageAspectRatio = imageWidth / imageHeight
-
+                    Log.d(TAG, "onResourceReady: ratio ${imageAspectRatio}")
+                    Log.d(TAG, "onResourceReady: ${imageViewHeight} ${imageViewWidth}")
                     // 이미지의 비율에 따라 캔버스에 맞게 크기 조정
                     val newImageWidth: Float
                     val newImageHeight: Float
@@ -110,7 +118,8 @@ class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
                     imageviewLetterPaper.layoutParams.height = newImageHeight.toInt()
                     imageviewLetterPaper.requestLayout()
 
-                    val horizontalPadding = resources.getDimensionPixelSize(com.intuit.sdp.R.dimen._32sdp)
+                    Log.d(TAG, "onResourceReady: ${newImageHeight} ${newImageWidth}")
+                    val horizontalPadding = resources.getDimensionPixelSize(com.intuit.sdp.R.dimen._34sdp)
                     val topPadding = resources.getDimensionPixelSize(com.intuit.sdp.R.dimen._60sdp)
                     val bottomPadding = resources.getDimensionPixelSize(com.intuit.sdp.R.dimen._20sdp)
 
@@ -139,10 +148,15 @@ class TypingWriteFragment : BaseFragment<FragmentTypingWriteBinding>(
 
     private fun initListener() = with(binding) {
         buttonSave.setOnClickListener {
-            val bitmap = viewToBitmap(imageviewLetterPaper)
-            writeLetterViewModel.setDrawingBitmap(bitmap)
-            writeLetterViewModel.setLetterText(edittextLetter.text.toString())
-            navController.navigate(TypingWriteFragmentDirections.actionTypingWriteFragmentToAddLetterImageFragment(LetterType.TYPING_WRITE))
+            if(edittextLetter.text.toString().trim().replace("\n","") == ""){
+                Toast.makeText(mainActivity, R.string.add_letter_image_no_text, Toast.LENGTH_LONG).show()
+            }else{
+                Log.d(TAG, "initListener: text ${edittextLetter.text.toString()}")
+                val bitmap = viewToBitmap(imageviewLetterPaper)
+                writeLetterViewModel.setDrawingBitmap(bitmap)
+                writeLetterViewModel.setLetterText(edittextLetter.text.toString())
+                navController.navigate(TypingWriteFragmentDirections.actionTypingWriteFragmentToAddLetterImageFragment(LetterType.TYPING_WRITE))
+            }
         }
     }
 
