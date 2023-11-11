b2b43f26194dc290afb9ff6e3771caded720da0a
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-12 03:08:48 +0900

    ✨ feat : 유저 검색 로직수정
    -검색창에 값 입력시 검색되도록 수정
    -검색창 값 비우는 버튼 추가
    -키보드 검색 버튼 클릭시 검색, 키보드 내리기 추가

--- Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/writeletter/selectuser/SelectUserFragment.kt
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/writeletter/selectuser/SelectUserFragment.kt
@@ -2,7 +2,12 @@ package com.zootopia.presentation.writeletter.selectuser
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -15,6 +20,7 @@ import com.zootopia.presentation.MainActivity
 import com.zootopia.presentation.R
 import com.zootopia.presentation.config.BaseFragment
 import com.zootopia.presentation.databinding.FragmentSelectUserBinding
+import com.zootopia.presentation.searchfriend.SearchFriendFragment
 import com.zootopia.presentation.writeletter.WriteLetterViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -38,12 +44,81 @@ class SelectUserFragment : BaseFragment<FragmentSelectUserBinding>(
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
         initData()
+        initView()
         initRecyclerView()
         initCollecter()
         initClickListener()
     }
 
     fun initData(){
+
+    }
+
+    fun initView()= with(binding) {
+        // toolbar setting
+//        toolbarHeartpathSearchFriend.apply {
+//            textviewCurrentPageTitle.text =
+//                resources.getString(R.string.toolbar_friend_search_title)
+//            imageviewBackButton.setOnClickListener {
+//                findNavController().popBackStack()
+//            }
+//        }
+
+        // 아이디 검색 창 edit text
+        editTextSearch.apply {
+            // text 변경 event
+            addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(sequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(
+                    sequence: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+                    // 만약에 값 없으면 X 버튼 gone, 입력 값 있으면 버튼 띄우기
+                    if (sequence != null) {
+                        if (sequence.isNotEmpty()) {
+                            imageviewInputCancelButton.visibility = View.VISIBLE
+                        } else {
+                            imageviewInputCancelButton.visibility = View.GONE
+                        }
+                        // 입력 값 갱신
+                        if(editTextSearch.text.toString() != ""){
+                            writeLetterViewModel.searchUser(editTextSearch.text.toString(), 10)
+                        }
+                    }
+                }
+                override fun afterTextChanged(p0: Editable?) {
+                }
+
+            })
+
+            // 입력한 키 확인 event
+            setOnKeyListener { view, keyCode, keyEvent ->
+                if (keyEvent.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    if(editTextSearch.text.toString() != ""){
+                        writeLetterViewModel.searchUser(editTextSearch.text.toString(), 10) // 검색하기
+                    }else{
+                         Toast.makeText(mainActivity, R.string.select_user_search_notice, Toast.LENGTH_LONG).show()
+                    }
+
+                    keyboardEvent() // 엔터 누르면 키보드 내리기
+                    setCancelButton()// 키보드 내리면 x button도 gone 시키기
+                    true
+                }
+                false
+            }
+
+            // focus 확인 event
+            setOnFocusChangeListener { view, gainFocus ->
+                if(gainFocus) {
+                    // focus를 받았을 때 값 있으면 x 버튼 visible 처리
+                    setButtonByFocus()
+                }
+            }
+        }
 
     }
 
@@ -90,7 +165,25 @@ class SelectUserFragment : BaseFragment<FragmentSelectUserBinding>(
             } else {
                 writeLetterViewModel.searchUser(editTextSearch.text.toString(), 10)
             }
+            setCancelButton()   // 검색하면 x 버튼 gone 처리
         }
+        imageviewInputCancelButton.setOnClickListener { 
+            editTextSearch.setText("")
+        }
+    }
+
+    private fun keyboardEvent() = with(binding){
+        val inputMethodManager = mainActivity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(editTextSearch.windowToken, 0)
+    }
+
+    private fun setCancelButton() = with(binding) {
+        imageviewInputCancelButton.visibility =  View.GONE
+    }
+
+    private fun setButtonByFocus() = with(binding) {
+        // focus를 받았을 때 값 있으면 x 버튼 visible 처리
+        if(editTextSearch.text.toString() != "") imageviewInputCancelButton.visibility = View.VISIBLE
     }
 
     override fun onDestroy() {
