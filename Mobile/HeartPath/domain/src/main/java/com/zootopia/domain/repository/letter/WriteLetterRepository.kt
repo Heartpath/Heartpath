3af130475d60cd05916acb2d350536f19798d7a4
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-11 22:32:28 +0900

    ✨ feat : 타이핑 편지 저장 api연결
    -관련 api service추가
    -관련 datasoruce 추가
    -관련 데이터 클래스, 매퍼추가
    -관련 레포지토리 수정
    -유스케이스 추가
    -뷰모델에 유스 케이스 연결
    -필요 string 리소스 추가

--- Mobile/HeartPath/domain/src/main/java/com/zootopia/domain/repository/letter/WriteLetterRepository.kt
+++ Mobile/HeartPath/domain/src/main/java/com/zootopia/domain/repository/letter/WriteLetterRepository.kt
@@ -2,10 +2,16 @@ package com.zootopia.domain.repository.letter
 
 import com.zootopia.domain.model.letter.UserLetterPaperDto
 import com.zootopia.domain.model.writeletter.HandLetterRequestDto
+import com.zootopia.domain.model.writeletter.TypingLetterRequestDto
 
 interface WriteLetterRepository {
     suspend fun postHandWriteLetter(
         handLetterRequestDto: HandLetterRequestDto,
+        content: String,
+        fileList: MutableList<String>
+    )
+    suspend fun postTypingWriteLetter(
+        typingLetterRequestDto: TypingLetterRequestDto,
         content: String,
         fileList: MutableList<String>
     )
