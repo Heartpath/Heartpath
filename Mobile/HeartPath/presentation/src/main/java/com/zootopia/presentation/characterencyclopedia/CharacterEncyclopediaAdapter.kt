08a689687f798efd23a5dc50f1328df2fd8f8190
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-11-12 05:06:13 +0900

    ✨ feat : 캐릭터 도감 화면 구성
    -캐릭터 도감에 필요한 리소스 추가
    -캐릭터 도감 리사이클러뷰 어댑터 추가
    -캐릭터 도감 프레그먼트 추가

---
+++ Mobile/HeartPath/presentation/src/main/java/com/zootopia/presentation/characterencyclopedia/CharacterEncyclopediaAdapter.kt
@@ -0,0 +1,45 @@
+package com.zootopia.presentation.characterencyclopedia
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zootopia.domain.model.store.CharacterDto
+import com.zootopia.presentation.databinding.ItemCharacterEncyclopediaBinding
+
+class CharacterEncyclopediaAdapter(var characterList: MutableList<CharacterDto>) :
+RecyclerView.Adapter<CharacterEncyclopediaAdapter.CharacterEncyclopediaViewHolder>(){
+
+    interface ItemClickListener{
+        fun onItemClicked(character: CharacterDto)
+    }
+    lateinit var itemClickListener: ItemClickListener
+
+    inner class CharacterEncyclopediaViewHolder(var binding: ItemCharacterEncyclopediaBinding):
+    RecyclerView.ViewHolder(binding.root){
+        fun setCharacter(character: CharacterDto){
+
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CharacterEncyclopediaViewHolder {
+        return CharacterEncyclopediaViewHolder(
+            ItemCharacterEncyclopediaBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return characterList.size
+    }
+
+    override fun onBindViewHolder(holder: CharacterEncyclopediaViewHolder, position: Int) {
+        holder.setCharacter(characterList[position])
+    }
+
+}
