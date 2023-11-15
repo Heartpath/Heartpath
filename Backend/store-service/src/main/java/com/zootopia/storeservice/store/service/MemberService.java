f86736fb30fedc02528181f2f5632861365f3d8f
HYEJIN <vvloveletter@naver.com>
2023-11-15 11:13:23 +0900

    ✏ modify : webclient->resttemplate 변경

--- Backend/store-service/src/main/java/com/zootopia/storeservice/store/service/MemberService.java
+++ Backend/store-service/src/main/java/com/zootopia/storeservice/store/service/MemberService.java
@@ -5,41 +5,69 @@ import com.zootopia.storeservice.common.error.exception.BadRequestException;
 import com.zootopia.storeservice.common.error.exception.ServerException;
 import com.zootopia.storeservice.store.dto.response.UserResDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import org.springframework.http.HttpStatus;
 
 @Service
 @Slf4j
 public class MemberService {
+//    public UserResDto accessTokenToMember(String accessToken) {
+//        WebClient webClient = WebClient.builder().build();
+//
+//        UserResDto res = webClient.get()
+//                .uri("http://3.34.86.93/api/user")
+//                .header(HttpHeaders.AUTHORIZATION, accessToken)
+//                .retrieve() // ResponseEntity를 받아 디코딩, exchange() : ClientResponse를 상태값, 헤더 제공
+//                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+//                    throw new BadRequestException(ErrorCode.INVALID_USER_REQUEST);
+//                })
+//                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+//                    throw new ServerException(ErrorCode.UNSTABLE_SERVER);
+//                })
+//                .bodyToMono(UserResDto.class)
+//                .block();
+//
+//        return res;
+//    }
     public UserResDto accessTokenToMember(String accessToken) {
-        WebClient webClient = WebClient.builder().build();
+        RestTemplate restTemplate = new RestTemplate();
 
-        UserResDto res = webClient.get()
-                .uri("http://3.34.86.93/api/user")
-                .header(HttpHeaders.AUTHORIZATION, accessToken)
-                .retrieve() // ResponseEntity를 받아 디코딩, exchange() : ClientResponse를 상태값, 헤더 제공
-                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
-                    throw new BadRequestException(ErrorCode.INVALID_USER_REQUEST);
-                })
-                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
-                    throw new ServerException(ErrorCode.UNSTABLE_SERVER);
-                })
-                .bodyToMono(UserResDto.class)
-                .block();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, accessToken);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        String apiUrl = "http://3.34.86.93/api/user";
+        ResponseEntity<UserResDto> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, UserResDto.class);
+
+        UserResDto res = responseEntity.getBody();
 
         return res;
     }
 
-    public String pointToMember(String memberId, int point){
-        WebClient webClient = WebClient.builder().build();
+//    public void pointToMember(String memberId, int point){
+//        WebClient webClient = WebClient.builder().build();
+//        log.info("들어옴?");
+//
+//        webClient.post()
+//                .uri("http://3.34.86.93/api/point/{memberId}/{point}", memberId, point)
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .block();
+//        log.info("보내짐??");
+//    }
+    public void pointToMember(String memberId, int point) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-        return webClient.post()
-                .uri("http://3.34.86.93/api/point/{memberId}/{point}", memberId, point)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        String url = "http://3.34.86.93/api/point/{memberId}/{point}";
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        restTemplate.postForObject(url, request, String.class, memberId, point);
+        log.info("보내짐??");
     }
 }
