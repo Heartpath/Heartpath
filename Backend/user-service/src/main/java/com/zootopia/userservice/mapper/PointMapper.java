679dc984891191dcb39178e025c7fd705da6cbec
김민수 <mskim.spring@gmail.com>
2023-11-15 10:01:55 +0900

    ✏ modify : 포인트 변경 Parameter 수정

--- Backend/user-service/src/main/java/com/zootopia/userservice/mapper/PointMapper.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/mapper/PointMapper.java
@@ -1,8 +1,8 @@
 package com.zootopia.userservice.mapper;
 
-import com.zootopia.userservice.dto.QueryParamMap;
 import com.zootopia.userservice.dto.UserPointTXDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -12,5 +12,8 @@ public interface PointMapper {
 
     List<UserPointTXDTO> readUserPointTransaction(String memberID);
 
-    int updateUserPoint(QueryParamMap paramMap);
+    int updateUserPoint(
+            @Param(value = "memberID") String memberID,
+            @Param(value = "point") int point
+    );
 }
