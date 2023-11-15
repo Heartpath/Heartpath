cbd64d8604df28276c48bed2d1db684105a26ffb
HYEJIN <vvloveletter@naver.com>
2023-11-15 15:38:55 +0900

    ✏ modify : log제거 및 token만료 오류 추가

--- Backend/store-service/src/main/java/com/zootopia/storeservice/store/service/MemberService.java
+++ Backend/store-service/src/main/java/com/zootopia/storeservice/store/service/MemberService.java
@@ -7,6 +7,7 @@ import com.zootopia.storeservice.store.dto.response.UserResDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -40,11 +41,14 @@ public class MemberService {
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         String apiUrl = "http://3.34.86.93/api/user";
-        ResponseEntity<UserResDto> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, UserResDto.class);
 
-        UserResDto res = responseEntity.getBody();
-
-        return res;
+        try {
+            ResponseEntity<UserResDto> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, UserResDto.class);
+            return responseEntity.getBody();
+        } catch (HttpClientErrorException.Unauthorized unauthorizedException) {
+            // Handle Unauthorized (401) error
+            throw new BadRequestException(ErrorCode.EXPIRED_TOKEN);
+        }
     }
 
 //    public void pointToMember(String memberId, int point){
@@ -62,11 +66,12 @@ public class MemberService {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        log.info("resttemplate는 되나요?");
 
         String url = "http://3.34.86.93/api/point/{memberId}/{point}";
 
         HttpEntity<String> request = new HttpEntity<>(headers);
-
+        log.info("이 위에도 찍혀?");
         restTemplate.postForObject(url, request, String.class, memberId, point);
         log.info("보내짐??");
     }
