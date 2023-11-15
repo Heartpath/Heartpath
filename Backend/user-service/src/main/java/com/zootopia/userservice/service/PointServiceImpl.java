d8bcced099ec8e77f1e20f35b481cc36b8977176
김민수 <mskim.spring@gmail.com>
2023-11-15 12:58:35 +0900

    ✏ modify : User Point 변경

--- Backend/user-service/src/main/java/com/zootopia/userservice/service/PointServiceImpl.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/service/PointServiceImpl.java
@@ -1,13 +1,16 @@
 package com.zootopia.userservice.service;
 
+import com.zootopia.userservice.domain.User;
 import com.zootopia.userservice.dto.UserPointTXDTO;
 import com.zootopia.userservice.mapper.PointMapper;
 import com.zootopia.userservice.mapper.UserMapper;
+import com.zootopia.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Slf4j
@@ -17,6 +20,7 @@ public class PointServiceImpl implements PointService {
 
     private final UserMapper userMapper;
     private final PointMapper pointMapper;
+    private final UserRepository userRepository;
 
     @Override
     public List<UserPointTXDTO> loadUserPointTransaction(String memberID) {
@@ -25,6 +29,11 @@ public class PointServiceImpl implements PointService {
 
     @Override
     public int reviseUserPoint(String memberID, int point) {
-        return userMapper.updateUserPoint(memberID, point);
+//        return userMapper.updateUserPoint(memberID, point);
+        User findMemberID = userRepository.findByMemberID(memberID).get();
+        findMemberID.setPoint(point);
+        userRepository.save(findMemberID);
+
+        return 1;
     }
 }
