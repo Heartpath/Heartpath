82001444955adb919e871878d449849946156fbd
김민수 <mskim.spring@gmail.com>
2023-11-16 17:31:21 +0900

    ⚙ config : change TX Isolation Level

--- Backend/user-service/src/main/java/com/zootopia/userservice/repository/UserRepository.java
+++ Backend/user-service/src/main/java/com/zootopia/userservice/repository/UserRepository.java
@@ -2,15 +2,20 @@ package com.zootopia.userservice.repository;
 
 import com.zootopia.userservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, String> {
 
+    @Transactional(readOnly = true)
     Optional<User> findByKakaoID(Long kakaoId);
 
+    @Transactional(readOnly = true)
     boolean existsByMemberID(String memberID);
 
+    @Transactional(readOnly = true)
     Optional<User> findByMemberID(String memberID);
 }
